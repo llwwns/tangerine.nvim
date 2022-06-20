483a1b5f324ae94a6f91987f3802380b9e01bd8a
llwwns <llwwns@gmail.com>
2022-06-20 10:41:26 +0900

    fix marker

--- fnl/tangerine/utils/diff.fnl
+++ fnl/tangerine/utils/diff.fnl
@@ -10,26 +10,21 @@
 ;; -------------------- ;;
 (lambda df.create-marker [source]
   "generates a comment tag from ftime of 'source'."
-  (let [base "-- :fennel:"
-        meta (vim.fn.getftime source)]
-    (.. base meta)))
+  "-- :fennel:generated")
 
 (lambda df.read-marker [path]
   "reads marker located in first 21 bytes of 'path'."
   (with-open [file (assert (io.open path "r"))]
     (local bytes  (or (file:read 21) ""))
-    (local marker (bytes:match ":fennel:([0-9]+)"))
-    (if marker
-        (tonumber marker)
-        :else false)))
+    (bytes:match ":fennel:generated")))
 
 (lambda df.stale? [source target]
   "compares marker of 'target' with ftime(source), true if source is stale."
   (if (not= 1 (vim.fn.filereadable target))
       (lua "return true"))
   (let [source-time (vim.fn.getftime source)
-        marker-time (df.read-marker target)]
-    (not= source-time marker-time)))
+        target-time (vim.fn.getftime target)]
+    (> source-time target-time)))
 
 
 :return df
--- lua/tangerine/utils/diff.lua
+++ lua/tangerine/utils/diff.lua
@@ -1,12 +1,10 @@
 local df = {}
 df["create-marker"] = function(source)
   _G.assert((nil ~= source), "Missing argument source on fnl/tangerine/utils/diff.fnl:11")
-  local base = "-- :fennel:"
-  local meta = vim.fn.getftime(source)
-  return (base .. meta)
+  return "-- :fennel:generated"
 end
 df["read-marker"] = function(path)
-  _G.assert((nil ~= path), "Missing argument path on fnl/tangerine/utils/diff.fnl:17")
+  _G.assert((nil ~= path), "Missing argument path on fnl/tangerine/utils/diff.fnl:15")
   local file = assert(io.open(path, "r"))
   local function close_handlers_8_auto(ok_9_auto, ...)
     file:close()
@@ -18,26 +16,19 @@ df["read-marker"] = function(path)
   end
   local function _2_()
     local bytes = (file:read(21) or "")
-    local marker = bytes:match(":fennel:([0-9]+)")
-    if marker then
-      return tonumber(marker)
-    elseif "else" then
-      return false
-    else
-      return nil
-    end
+    return bytes:match(":fennel:generated")
   end
   return close_handlers_8_auto(_G.xpcall(_2_, (package.loaded.fennel or debug).traceback))
 end
 df["stale?"] = function(source, target)
-  _G.assert((nil ~= target), "Missing argument target on fnl/tangerine/utils/diff.fnl:26")
-  _G.assert((nil ~= source), "Missing argument source on fnl/tangerine/utils/diff.fnl:26")
+  _G.assert((nil ~= target), "Missing argument target on fnl/tangerine/utils/diff.fnl:21")
+  _G.assert((nil ~= source), "Missing argument source on fnl/tangerine/utils/diff.fnl:21")
   if (1 ~= vim.fn.filereadable(target)) then
     return true
   else
   end
   local source_time = vim.fn.getftime(source)
-  local marker_time = df["read-marker"](target)
-  return (source_time ~= marker_time)
+  local target_time = vim.fn.getftime(target)
+  return (source_time > target_time)
 end
 return df
